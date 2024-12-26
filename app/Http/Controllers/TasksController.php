6fa0512159f0bc7debfb30d161020bb826a6b856
luongthitam <luong_thi_tam@gdo.co.jp>
2024-12-26 16:49:57 +0900

    kadai-tasklist

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('id', 'asc')->paginate(10);
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $task = new Task;
+
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'content' => 'required|max:255',
+        ]);
+        $task = Task::findOrFail($id);
+
+        $task->content = $request->content;
+        $task->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return redirect('/');
+    }
+}
